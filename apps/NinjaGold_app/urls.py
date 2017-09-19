8f9fe3a42e475a06a1d512a7aa399e1ca98a6d8b
JamesORourke <geekjamesorourkejr@icloud.com>
2017-09-18 21:29:55 -0700

    Done Version 2

--- apps/NinjaGold_app/urls.py
+++ apps/NinjaGold_app/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
 url(r'^$', views.index),
+url(r'^process_money$', views.process_money),
+url(r'^reset$', views.reset),
 ]
--- apps/NinjaGold_app/views.py
+++ apps/NinjaGold_app/views.py
@@ -1,8 +1,53 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect
-from models import *
 from django.contrib import messages
-import bcrypt
+from random import random, randint
+from datetime import datetime
 def index(request):
+    try:
+        yourgold = request.session['yourGold']
+        history = request.session['history']
+    except Exception as e:
+        return redirect('/reset')
     context = { }
     return render(request, "NinjaGold_app/index.html", context)
+
+def process_money(request, methods=['POST']):
+    try:
+        yourgold = request.session['yourGold']
+        # history = request.session['history']
+        building = request.POST['building']
+    except Exception as e:
+        print e.message
+        messages.error(request, 'You must do this from the home page.' + e.message)
+        return redirect('/')
+    low = 10
+    high = 20
+    resultMsg = ''
+    theTime = datetime.now()
+    if building == 'Farm':
+        low = 10
+        high = 20
+    elif building == 'Cave':
+        low = 5
+        high = 10
+    elif building == 'House':
+        low = 2
+        high = 5
+    else:   # building == 'Casino':
+        low = -50
+        high = 50
+    earnedGold = randint(low, high)
+    yourgold += earnedGold
+    request.session['yourGold'] += earnedGold
+    if earnedGold >= 0:
+        resultMsg = "<div class='green'>Earned {} gold from the {}!<//div>".format(earnedGold, building, theTime)
+    else:
+        resultMsg = "<div class='red'>Entered a {} and lost {} gold!<//div>".format(building, -1 * earnedGold, theTime)
+    request.session['history'].append(resultMsg)
+    return redirect('/')
+
+def reset(request, methods=['POST']):
+    request.session['yourGold'] = 0
+    request.session['history'] = []
+    return redirect('/')
